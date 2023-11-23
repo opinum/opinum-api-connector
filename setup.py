ec1c69c4ab02b2457f7993112a660515db014378
watteyne <patrick@watteyne.com>
2023-11-23 16:29:10 +0100

    Wrong folder structure

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="opinum-api-connector",
-    version="1.1.0",
+    version="1.1.1",
     author="Patrick Watteyne",
     author_email="patrick@watteyne.com",
     description="Package to interact with the Opinum API",
