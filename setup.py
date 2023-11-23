759da0091caa8d77fb866440016bd312c8ac89e7
watteyne <patrick@watteyne.com>
2023-11-23 14:29:00 +0100

    Util for a site duplication

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="opinum-api-connector",
-    version="1.0.2",
+    version="1.1.0",
     author="Patrick Watteyne",
     author_email="patrick@watteyne.com",
     description="Package to interact with the Opinum API",
